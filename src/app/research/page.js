2dee9fad64caaed86e7072d0028d60d07951df28
cartgr <carterblair@uvic.ca>
2023-12-21 15:28:39 -0700

    Home page initialized

---
+++ src/app/research/page.js
@@ -0,0 +1,10 @@
+export default function Projects() {
+    return (
+        <div className="flex min-h-screen bg-neutral-50">
+            {/* Main content area */}
+            <main className="flex-1 p-24">
+                {/* Initially blank, you can add content here */}
+            </main>
+        </div>
+    )
+}
