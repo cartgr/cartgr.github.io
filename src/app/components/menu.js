f7cc56430f62498b202daf04a8977aba71889f9e
cartgr <carterblair@uvic.ca>
2024-03-28 14:45:48 -0400

    update links in menu to use next Link

--- src/app/components/menu.js
+++ src/app/components/menu.js
@@ -2,6 +2,8 @@
 
 import React, { useState, useEffect } from 'react';
 import { Bars3Icon } from '@heroicons/react/24/outline';
+// use the next Link component to navigate between pages
+import Link from 'next/link';
 
 const Menu = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -35,24 +37,24 @@ const Menu = () => {
             {/* Navigation links as buttons */}
             <ul className={`absolute sm:relative bg-neutral-100 sm:bg-transparent w-full sm:w-auto transition-opacity duration-300 ease-in-out ${isMenuOpen ? 'opacity-100' : 'opacity-0'} ${showMenu ? 'visible' : 'invisible'} sm:opacity-100 sm:visible ${showMenu ? 'top-16' : ''} left-0 flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4 mr-8 py-4 sm:py-0 px-4 sm:px-0`}>
                 <li>
-                    <a href="/website2.0" className="flex items-center px-4 py-2 bg-neutral-50 text-neutral-800 rounded-md shadow hover:shadow-lg transition duration-300 ease-in-out">
+                    <Link href="/" className="flex items-center px-4 py-2 bg-neutral-50 text-neutral-800 rounded-md shadow hover:shadow-lg transition duration-300 ease-in-out">
                         Home
-                    </a>
+                    </Link>
                 </li>
                 <li>
-                    <a href="/website2.0/research" className="flex items-center px-4 py-2 bg-neutral-50 text-neutral-800 rounded-md shadow hover:shadow-lg transition duration-300 ease-in-out">
+                    <Link href="/research" className="flex items-center px-4 py-2 bg-neutral-50 text-neutral-800 rounded-md shadow hover:shadow-lg transition duration-300 ease-in-out">
                         Research
-                    </a>
+                    </Link>
                 </li>
                 <li>
-                    <a href="/website2.0/books" className="flex items-center px-4 py-2 bg-neutral-50 text-neutral-800 rounded-md shadow hover:shadow-lg transition duration-300 ease-in-out">
+                    <Link href="/books" className="flex items-center px-4 py-2 bg-neutral-50 text-neutral-800 rounded-md shadow hover:shadow-lg transition duration-300 ease-in-out">
                         Books
-                    </a>
+                    </Link>
                 </li>
                 <li>
-                    <a href="/website2.0/links" className="flex items-center px-4 py-2 bg-neutral-50 text-neutral-800 rounded-md shadow hover:shadow-lg transition duration-300 ease-in-out">
+                    <Link href="/links" className="flex items-center px-4 py-2 bg-neutral-50 text-neutral-800 rounded-md shadow hover:shadow-lg transition duration-300 ease-in-out">
                         Links & Posts
-                    </a>
+                    </Link>
                 </li>
             </ul>
         </>
