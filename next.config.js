cec4ea90b0b72bd467714778a4510524afeee04b
cartgr <carterblair@uvic.ca>
2025-05-31 16:12:34 -0600

    add redirect from old url

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
     output: "export",
     images: { unoptimized: true },
     // Remove basePath and assetPrefix since we're now using cartgr.github.io directly
+    async redirects() {
+        return [
+            {
+                source: '/website/:path*',
+                destination: '/:path*',
+                permanent: true,
+            },
+        ]
+    },
 }
 
 module.exports = nextConfig;
